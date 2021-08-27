3b414aa3a297e78dde76a2d7f2b2138d941f930c
masozow <14838605+masozow@users.noreply.github.com>
2021-08-26 21:16:26 -0600

    adding more props

--- src/functionality/IconNameSelection.js
+++ src/functionality/IconNameSelection.js
@@ -1,5 +1,6 @@
 import { faSun, faCloudSun, faCloud, faCloudRain, faCloudSunRain, faCloudMoonRain, faPooStorm, faSnowflake, faSmog, faRainbow, faMoon } from "@fortawesome/free-solid-svg-icons";
-
+///iconRelations keys according to openweather API available icons, and values
+///according to  available icons in fontawesome free package
 const iconRelations = {
     '01d': faSun,
     '01n': faMoon,
@@ -18,29 +19,30 @@ const iconRelations = {
     '13n': faSnowflake,
     '50d': faSmog,
     '50n': faSmog,
+    'default': faRainbow
 }
 export function iconNameSelection(iconName = '', icons = iconRelations) {
-    if (iconRelations[iconName]) {
+    if (iconRelations[iconName] && iconName !== 'default') {
         return iconRelations[iconName];
     }
     else {
-        return faRainbow;
+        return iconRelations['default'];
     }
 }
 
 /*
-            icons:
-            openweather=fontAwesome
-            01d=faSun
-            02d=faCloudSun
-            03d=03n=faCloud
-            04d=04n=faClouds(just faCloud, bc faClouds is pro)
-            09d=09n=faCloudRain
-            10d=faCloudSunRain
-            11d=11n=faPooStorm
-            13d=13n=faSnowFlake
-            50d=50n=faSmog (bc fog is pro)
-            01n=faMoon
-            02n=faCloudMoon
-            10n=faCloudMoonRain
-            */
+    icons:
+    openweather=fontAwesome
+    01d=faSun
+    02d=faCloudSun
+    03d=03n=faCloud
+    04d=04n=faClouds(just faCloud, bc faClouds is pro)
+    09d=09n=faCloudRain
+    10d=faCloudSunRain
+    11d=11n=faPooStorm
+    13d=13n=faSnowFlake
+    50d=50n=faSmog (bc fog is pro)
+    01n=faMoon
+    02n=faCloudMoon
+    10n=faCloudMoonRain
+*/
