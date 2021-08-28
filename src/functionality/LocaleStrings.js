198c18600464f3cad9f461666989e45dc65bd14e
masozow <14838605+masozow@users.noreply.github.com>
2021-08-27 23:29:51 -0600

    trying to show error

--- src/functionality/LocaleStrings.js
+++ src/functionality/LocaleStrings.js
@@ -7,7 +7,7 @@ const monthNameLocales = {
     'en': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
     'es': ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 }
-export function weekdays(day = 0, locale = 'en') {
+export function weekdays(day, locale = 'en') {
     if (day >= 0 && day < 7) {
         if (weekdaysLocales[locale][day]) {
             return weekdaysLocales[locale][day];
@@ -22,7 +22,7 @@ export function weekdays(day = 0, locale = 'en') {
 };
 
 
-export function monthName(month = 0, locale = 'en') {
+export function monthName(month, locale = 'en') {
     if (month >= 0 && month < 12) {
         if (monthNameLocales[locale][month]) {
             return monthNameLocales[locale][month];
