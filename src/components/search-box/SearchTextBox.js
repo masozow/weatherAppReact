05d19746a790bb17a598405b4cb8a54721add3ec
masozow <14838605+masozow@users.noreply.github.com>
2022-09-23 00:05:17 -0600

    partially fixed textbox and modal styling. modal needs more work

--- src/components/search-box/SearchTextBox.js
+++ src/components/search-box/SearchTextBox.js
@@ -1,16 +1,19 @@
 import classNames from 'classnames';
 import styles from './searchBox.module.css';
-import { forwardRef } from 'react';
+import { forwardRef, useContext } from 'react';
 import { useEffect } from 'react/cjs/react.development';
+import WeatherContext from '../../store/WheaterContext';
 // import { useCallback } from 'react/cjs/react.production.min';
 
 const SearchTextBox = forwardRef((props, ref) => {
+    const weatherContext = useContext(WeatherContext);
+
     useEffect(() => {
         ref.current.focus();
     }, [ref])
 
 
-    const searchTextBoxStyles = classNames(styles.textBox, { [styles.textBox_open]: props.textBoxIsVisible });
+    const searchTextBoxStyles = classNames(styles.textBox, { [styles.textBoxLight]: weatherContext.theme }, { [styles.textBox_open]: props.textBoxIsVisible });
     return <input type="text" placeholder="Search for a city" ref={ref} className={searchTextBoxStyles} />
 });
 
