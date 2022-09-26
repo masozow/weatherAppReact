9ee2c32c7ec9c1a6e50ac055f51e039f2e0c2b40
masozow <14838605+masozow@users.noreply.github.com>
2022-09-25 22:54:20 -0600

    changing useEffect source in SearchTextBox component, because it was imported from reactcs.development something, instead from react

--- src/components/search-box/SearchTextBox.js
+++ src/components/search-box/SearchTextBox.js
@@ -1,17 +1,16 @@
 import classNames from 'classnames';
 import styles from './searchBox.module.css';
-import { forwardRef, useContext } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import { forwardRef, useContext, useEffect } from 'react';
 import WeatherContext from '../../store/WheaterContext';
 // import { useCallback } from 'react/cjs/react.production.min';
 
 const SearchTextBox = forwardRef((props, ref) => {
-    const weatherContext = useContext(WeatherContext);
 
     useEffect(() => {
         ref.current.focus();
     }, [ref])
 
+    const weatherContext = useContext(WeatherContext);
 
     const searchTextBoxStyles = classNames(styles.textBox, { [styles.textBoxLight]: weatherContext.theme }, { [styles.textBox_open]: props.textBoxIsVisible });
     return <input type="text" placeholder="Search for a city" ref={ref} className={searchTextBoxStyles} />
